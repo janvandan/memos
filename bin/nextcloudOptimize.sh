78ff8c2f376aa18b3f22aadd679bda3f6f52da18
Van Dravik <jan@vandravik.com>
2019-12-26 09:34:59 +0000

    suppression nextcloudOptimize.sh

--- bin/nextcloudOptimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-redis-cli <<EOF
-FLUSHALL
-quit
-EOF
-sudo -u www-data php /var/www/html/nextcloud/occ files:scan --all
-sudo -u www-data php /var/www/html/nextcloud/occ files:scan-app-data
