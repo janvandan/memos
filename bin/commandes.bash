7f0fcd41bd4ae28a40fdaa123e5ad35d29e817ab
Van Dravik <jan@vandravik.com>
2020-07-22 10:41:11 +0200

    Update commandes.bash

--- bin/commandes.bash
+++ bin/commandes.bash
@@ -7,3 +7,7 @@ ls | while read i; do j=$(echo "$i" | tr -d [:space:]); mv "$i" "$j";done
 # chgt de nom de fichiers
 
 ls | while read i; do j=$(echo "$i" | cut -d . -f 1); mv "$i" "$j""-1024x768.jpg";done
+
+# chgt le début du nom
+
+ls | while read i; do j=$(echo "$i" | cut -c 6-); mv "$i" "David-$j";done
