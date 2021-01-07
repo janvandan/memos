d0b19abd2b5704351b107bfcf6362eb50fd5e094
Van Dravik <jan@vandravik.com>
2021-01-07 17:27:06 +0100

    Update xpath.cmd

--- bin/xpath.cmd
+++ bin/xpath.cmd
@@ -5,6 +5,7 @@ cd
 ls
 cat
 cat key[2]
+cat *[position()<10]
 xpath
 help
 pwd
@@ -14,3 +15,10 @@ xpath .
 xpath //root
 xpath *
 xpath *[position()<10]
+
+xpath /plist/dict/array[1]/dict[4]/key[text()='AlbumName']/following::string[1]
+
+xpath //key[text()='AlbumName']/following::string[1]/text()
+
+xpath /plist/dict/array[1]/dict/key[text()='AlbumName']/following::string[1]/text()
+xpath /plist/dict/array[1]/dict/key[text()='AlbumName']/following::string[1]/text()
