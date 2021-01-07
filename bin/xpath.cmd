21f0666d1dd4d2beda21a12766f25ba37ac4548b
Van Dravik <jan@vandravik.com>
2021-01-07 13:16:35 +0100

    Create xpath.cmd

---
+++ bin/xpath.cmd
@@ -0,0 +1,16 @@
+# commande pour parser xml via xmllint --shell par exemple
+
+# commande xmllint
+cd
+ls
+cat
+cat key[2]
+xpath
+help
+pwd
+
+# sur l'exemple xml AlbumData.xml de iPhoto
+xpath .
+xpath //root
+xpath *
+xpath *[position()<10]
